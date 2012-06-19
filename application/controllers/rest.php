9b18c94a6fe8d58279bf42db5e56d01592915112
James Ming <jamesming@gmail.com>
2012-06-19 06:07:05 -0700

    added

---
+++ application/controllers/rest.php
@@ -0,0 +1,275 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Rest extends CI_Controller {
+	
+	function __construct(){
+      parent::__construct();
+		
+			$this->table = $this->uri->segment(3);	
+			
+			$this->id = $this->uri->segment(4);	
+	}
+
+	/* index
+	*
+	*		
+	*  Usage:  /api/index.php/rest/index/:table/:id
+	*
+	*
+	*/
+	function index(){
+
+
+				
+		$request_method = strtolower($_SERVER['REQUEST_METHOD']);
+		
+		switch ($request_method){
+			
+			case 'get':
+			
+				if( $this->id ){
+					
+					$data = $this->input->get();
+					
+					$categories = $this->query->get_table_by_id( $this->table, $this->id );	
+										
+				}else{
+					
+					$data = $this->input->get();
+					
+					$categories = $this->query->get_table_rows( $this->table );					
+					
+				};
+				
+				echo json_encode(  $categories  );
+				
+				break;
+				
+			case 'post':
+			
+				$this->my_database_model->create_generic_table($this->table );
+			
+				$parameters = json_decode(file_get_contents('php://input'), true);
+				
+				foreach( $parameters  as  $key => $value){
+					$set_what[$key] = $value;
+				};
+				
+				$post_array['table'] = $this->table;
+				
+				$post_array['set_what'] = $set_what;
+			 
+				echo $this->query->insert( $post_array );
+				
+				break;
+				
+			case 'put':
+			
+				$parameters = json_decode(file_get_contents('php://input'), true);
+				
+				foreach( $parameters  as  $key => $value){
+					$key == 'id' || $set_what_array[$key] = $value;
+				};
+				
+				$this->query->put_table_by_id( 
+									$this->table, 
+									$this->id  = $parameters['id'],
+									$set_what_array
+						 );
+
+				
+				break;
+				
+			case 'delete':
+				
+				$this->my_database_model->delete_from_table(
+				$this->table, 
+				$where_array = array(
+					'id' => $this->id
+				));
+				
+				$this->remove();
+				
+				break;				
+				
+		}		
+		
+	}
+	
+	
+	function upload(){
+		
+		$prefix_file_name = 'file';
+		
+		$destination_filename = $prefix_file_name . '.png';
+		
+		$this->my_database_model->create_generic_table($this->table );
+		
+		$path_array = array(
+			'folder'=> $this->table,
+			'image_id' => $this->id
+		);
+
+		$upload_path = $this->tools->set_directory_for_upload( $path_array );
+		
+
+		$config['upload_path'] = './' . $upload_path;
+		$config['allowed_types'] = 'bmp|jpeg|gif|jpg|png';
+		$config['overwrite'] = 'TRUE';
+		$config['file_name'] = $destination_filename;
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload("file_name")){
+					echo $this->upload->display_errors();
+					exit;
+		}
+		else{
+			
+			$original_path = $upload_path  . '/' . $destination_filename;
+			
+			$this->resize(
+				$original_path, 
+				$suffix = '_thumb', 
+				$target_width = $this->input->post('thumb_width'), 
+				$target_height = $this->input->post('thumb_height')
+			);
+			
+			$thumb_path = $upload_path  . '/' . $prefix_file_name . '_thumb.png';
+			
+			$this->resize(
+				$original_path, 
+				$suffix = '_full', 
+				$target_width = $this->input->post('full_width'), 
+				$target_height = $this->input->post('full_height')
+			);
+			
+			$full_path = $upload_path  . '/' . $prefix_file_name . '_full.png';				
+			
+		?>
+
+<script type="text/javascript" language="Javascript">
+	
+<?php     
+
+$image_information = getimagesize($thumb_path);
+$width_of_file = $image_information[0];
+$height_of_file = $image_information[1];
+$marginLeft = $width_of_file /2;
+$marginTop  = $height_of_file /2;										
+
+?>
+
+window.parent.<?php  echo $this->input->post('php_callback_dom_el');   ?>
+.css({
+	width:'<?php  echo $width_of_file   ?>', 
+	height: '<?php  echo $height_of_file   ?>', 
+	'margin-left':'-<?php echo $marginLeft;    ?>px', 
+	'margin-top':'-<?php echo $marginTop;    ?>px'
+})
+.attr({
+		id	: <?php  echo $this->id   ?>
+	 ,src: '<?php  echo base_url() . $thumb_path . "?random=" . rand(5,124344523)   ?>'
+	
+});
+
+window.parent.$('#launchModal').click();
+								
+
+</script>
+
+<?php
+
+		}		
+		
+	}
+	
+	function remove(){
+		
+		$dir_path = 'uploads/' . $this->table . '/' 
+		. $this->id . '/';
+		
+		$this->tools->recursiveDelete($dir_path);
+		
+		
+	}
+	
+	
+	function resize($path, $suffix, $target_width, $target_height){
+		
+			$image_information = getimagesize($path);
+			
+ 			$width_of_file = $image_information[0];
+			$height_of_file = $image_information[1];
+			
+			if( $width_of_file > $height_of_file){
+				
+				$this->tools->clone_and_resize_append_name_of(
+					$suffix, 
+					$path, 
+					$target_width,
+					$height = $this->tools->get_new_size_of (
+												$what = 'height', 
+												$target_width, 
+												$width_of_file, 
+												$height_of_file 
+												)
+					);				
+					
+			}else{
+				
+				$this->tools->clone_and_resize_append_name_of(
+					$suffix, 
+					$path, 
+					$width = $this->tools->get_new_size_of (
+												$what = 'width', 
+												$target_height, 
+												$width_of_file, 
+												$height_of_file 
+												), 
+					$target_height
+					);				
+				
+			};
+
+
+		
+	}
+
+	/* create_table
+	*
+	*  
+	*
+	*/
+	function create_table(){
+		
+		$this->my_database_model->create_generic_table($this->table );
+		
+		
+		$fields_array = array(
+		                      'name' => array(
+		                                               'type' => 'varchar(255)'
+		                                    )/*,
+		                      'image_type_id' => array(
+		                                               'type' => 'int(11)'
+		                                    ),*/
+		//                      'county' => array(
+		//                                               'type' => 'varchar(255)'
+		//                                    ),
+		//                      'city' => array(
+		//                                               'type' => 'varchar(255)'
+		//                                    ),
+		//                      'state' => array(
+		//                                               'type' => 'varchar(255)'
+		//                                    )
+		              ); 
+	              
+		$this->my_database_model->add_column_to_table_if_not_exist(
+			$this->table, 
+			$fields_array
+		);
+	   
+	
+	}
+
+}
---
+++ application/libraries/Query.php
@@ -0,0 +1,99 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+ 
+class Query {
+
+	private $CI;	
+	
+	function query(){
+		
+		$this->CI =& get_instance();	
+		
+	}
+	
+	
+	function get_table_by_id( $table, $id){
+		
+		return $this->CI->tools->object_to_array($this->CI->my_database_model->select_from_table( 
+			$table, 
+			$select_what = '*',    
+			array(
+				'id' => $id
+			), 
+			$use_order = FALSE, 
+			$order_field = '', 
+			$order_direction = 'asc', 
+			$limit = -1, 
+			$use_join = FALSE
+			));
+		
+	}	
+	
+	
+	function get_table_rows( $table){
+		
+		return $this->CI->tools->object_to_array($this->CI->my_database_model->select_from_table( 
+			$table, 
+			$select_what = '*',    
+			array(), 
+			$use_order = FALSE, 
+			$order_field = '', 
+			$order_direction = 'asc', 
+			$limit = -1, 
+			$use_join = FALSE
+			));
+		
+	}
+	
+	function put_table_by_id( $table, $id, $set_what_array){
+		
+		return $this->CI->my_database_model->update_table_where(
+					$table, 
+					$where_array = array('id' => $id),
+					$set_what_array
+					);
+		
+	}	
+
+	function insert($post_array){
+
+		$this->add_column_if_not_exist($post_array['table'], $post_array['set_what']);		
+		
+		return $this->CI->my_database_model->insert_table(
+									$post_array['table'], 
+									$post_array['set_what']
+									); 					
+
+	}
+
+	function update($post_array){
+
+		$this->add_column_if_not_exist($post_array['table'],  $post_array['set_what']);
+
+		return $this->CI->my_database_model->update_table_where(
+					$post_array['table'], 
+					$where_array = array('id'=>$post_array['id']),
+					$post_array['set_what']
+					);	
+	}
+	
+
+	
+	function add_column_if_not_exist($table, $set_what_array){
+		
+			foreach( $set_what_array  as  $key => $value){
+				$fields_array = array(
+							$key => array('type' => 'varchar(255)')                                          
+            	); 
+
+				$this->CI->my_database_model->add_column_to_table_if_not_exist(
+					$table, 
+					$fields_array
+				);    					
+			};
+ 	
+		
+	}
+	
+}
